40c191865e1f663974f3b65df6097a422b00faed
Nico <nicoandrade@gmail.com>
2015-05-25 03:15:42 -0300

    1.0.0

--- framework/functions/custom-functions.php
+++ framework/functions/custom-functions.php
@@ -7,4 +7,14 @@
 		}
 		add_action( 'wp_head', 'ql_render_title' );
 	endif;
+
+	//Change the Read More link
+
+	if ( ! function_exists( 'ql_modify_read_more_link' ) ) :
+		function ql_modify_read_more_link() {
+			return '<a class="more-link btn btn-ql" href="' . esc_url( get_permalink() ) . '">'. __('Read More', 'eneaa') . '</a>';
+		}
+	endif;
+	add_filter( 'the_content_more_link', 'ql_modify_read_more_link' );
+
 ?>
