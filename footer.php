c2a4c11814c05a0357612e19f6090e0c2d7927e0
Nico <nicoandrade@gmail.com>
2015-07-06 12:00:35 -0300

    1.0.10
    changed footer link

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <p><?php esc_html_e( 'Proudly powered by ', 'vito' ); ?><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'vito' ) ); ?>">WordPress</a>. <?php esc_html_e( 'Vito theme designed by ', 'vito' ); ?><a rel="nofollow" href="<?php echo esc_url( __( 'https://quemalabs.com/', 'vito' ) ); ?>">Quema Labs</a>.</p>
+                    <p><?php esc_html_e( 'Proudly powered by ', 'vito' ); ?><a href="<?php echo esc_url( __( 'https://wordpress.org/', 'vito' ) ); ?>">WordPress</a>. <?php esc_html_e( 'Vito theme designed by ', 'vito' ); ?><a rel="nofollow" href="<?php echo esc_url( __( 'http://quemalabs.com/', 'vito' ) ); ?>">Quema Labs</a>.</p>
                 </div>
             </div>
         </div>
